0cb750acfeeae2131ae35d176bb22fd12035eb54
itsloraee <laurxee@gmail.com>
2025-12-14 14:41:21 -0400

    Creation banniere pub (en cours)

---
+++ app/Filament/Resources/PromotionalMessages/Pages/EditPromotionalMessage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\PromotionalMessages\Pages;
+
+use App\Filament\Resources\PromotionalMessages\PromotionalMessageResource;
+use Filament\Actions\DeleteAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPromotionalMessage extends EditRecord
+{
+    protected static string $resource = PromotionalMessageResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+        ];
+    }
+}
---
+++ app/Filament/Resources/PromotionalMessages/Schemas/PromotionalMessageForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\PromotionalMessages\Schemas;
+
+use Filament\Schemas\Schema;
+
+class PromotionalMessageForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
